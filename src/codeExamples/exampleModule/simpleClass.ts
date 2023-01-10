2f89a491d99f64c3690a61d76ff4bb0e0d50aa0b
Scott Morse <scottmorsedev@gmail.com>
2023-01-09 23:00:03 -0600

    Add simple class

---
+++ src/codeExamples/exampleModule/simpleClass.ts
@@ -0,0 +1,14 @@
+export class SimpleClass {
+  add(a: number, b: number) {
+    return a + b;
+  }
+  subtract(a: number, b: number) {
+    return a - b;
+  }
+  divide(a: number, b: number) {
+    return a / b;
+  }
+  multiply(a: number, b: number) {
+    return a * b;
+  }
+}
